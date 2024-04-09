086c40cf1013f9c2ab78bde3fa90ad4d2d6bff96
MoneyFool9 <jiahqian@gmail.com>
2024-04-09 23:29:47 +0800

    添加js文件，实现滚动置顶

--- js/exercism.js
+++ js/exercism.js
@@ -1,3 +1,21 @@
 window.addEventListener('load', function () {
-    
+    window.onscroll = function () { scrollFunction() };
+
+    function scrollFunction() {
+        var scrollToTopBtn = document.getElementById("scrollToTop");
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            scrollToTopBtn.style.display = "block";
+        } else {
+            scrollToTopBtn.style.display = "none";
+        }
+    }
+
+    document.getElementById("scrollToTop").addEventListener("click", function () {
+        scrollToTop();
+    });
+
+    function scrollToTop() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0; 
+    }
 })
